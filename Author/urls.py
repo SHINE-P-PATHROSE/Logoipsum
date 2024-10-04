5043b78d922a76d0fb77bfb32ea785308e3f5045
Shine <shineppathrose2002@gmail.com>
2024-10-04 14:14:57 +0530

    Your commit message here

--- Author/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.urls import path
-# from . import views
-#
-# urlpatterns = [
-#     path('authors/', views.author_list, name='author_list'),
-#     path('authors/add/', views.add_author.css, name='add_author.css'),
-#     path('authors/edit/<int:author_id>/', views.edit_author, name='edit_author'),
-#     path('authors/<int:author_id>/', views.author_details, name='author_details'),
-# ]
--- Logoipsum/urls.py
+++ Logoipsum/urls.py
@@ -19,6 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('Author/', include('Author.urls')),
     path('', include('Book.urls')),
 ]
